5ff78aed97839a72b82d6aafaa76a4c5533c0ecc
unknown <Reejan.Katwal@metropolia.fi>
2026-03-12 14:09:39 +0200

    Bug fix

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -40,7 +40,7 @@ const updateWorkout = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "No such workout" });
+    return res.status(404).json({ error: "No such workout" });
   }
 
   const workout = await Workout.findOneAndUpdate(
